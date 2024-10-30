aea5ac94e4682b83c7bab01cc40a69551bed2e74
Rachel Dooley <102691550+Rae1821@users.noreply.github.com>
2024-10-30 13:36:38 -0400

    WIP: Reworking API call to server side

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,12 +1,43 @@
-import AllProducts from "@/components/AllProducts";
+// import AllProducts from "@/components/AllProducts";
+import ClothingCard from "@/components/ClothingCard";
+import ProductSearch from "@/components/ProductSearch";
+import { fetchClothing } from "@/utils/helper";
 
-export default async function ProductsPage() {
-  const searchParams = {}; // Add the required searchParams property here
+// export async function getServerSideProps(context: any) {
+//   const { searchParams } = context.query;
+//   const data = await fetchClothing(searchParams);
+
+//   return {
+//     props: {
+//       data,
+//       searchParams,
+//     },
+//   };
+// }
+
+const ProductsPage = async ({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | string[] }>;
+}) => {
+  const query = await searchParams;
+  const items = await fetchClothing(query);
 
   return (
     <div className="container">
-      <AllProducts searchParams={searchParams} />{" "}
-      {/* Pass the searchParams property to the AllProducts component */}
+      {/* <AllProducts searchParams={searchParams} />{" "}
+      Pass the searchParams property to the AllProducts component */}
+      <div>
+        <ProductSearch />
+      </div>
+
+      <div>
+        {items.map((item: any) => (
+          <ClothingCard key={item.id} clothing={item} />
+        ))}
+      </div>
     </div>
   );
-}
+};
+
+export default ProductsPage;
--- components/ProductSearch.tsx
+++ components/ProductSearch.tsx
@@ -2,8 +2,8 @@
 
 // import { Input } from "../ui/input";
 // import { AddSearchButton } from "./AddSearchButton";
-import React, { useState } from "react";
-import { useRouter } from "next/navigation";
+import React, { useCallback, useState } from "react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { Button } from "./ui/button";
 import Image from "next/image";
 import { useFormStatus } from "react-dom";
@@ -14,6 +14,19 @@ const ProductSearch = () => {
   const { pending } = useFormStatus();
 
   const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const createQueryString = useCallback(
+    (name: string, value: string) => {
+      const params = new URLSearchParams(searchParams.toString());
+      params.set(name, value);
+
+      return params.toString();
+    },
+
+    [searchParams]
+  );
 
   const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -22,27 +35,27 @@ const ProductSearch = () => {
       return alert("Please fill in the search bar");
     }
 
-    updateSearchParams(searchItem.toLowerCase());
+    router.push(pathname + "?" + createQueryString("searchItem", searchItem));
   };
 
-  const updateSearchParams = (searchItem: string) => {
-    // create a new URLSearchParams object using the current URL search parameters
-    const searchParams = new URLSearchParams(window.location.search);
+  // const updateSearchParams = (searchItem: string) => {
+  //   // create a new URLSearchParams object using the current URL search parameters
+  //   const searchParams = new URLSearchParams(window.location.search);
 
-    // update or delete the 'searchitem' search parameter bades on the 'searchitem' value
-    if (searchItem) {
-      searchParams.set("searchItem", searchItem);
-    } else {
-      searchParams.delete("searchItem", searchItem);
-    }
+  //   // update or delete the 'searchitem' search parameter bades on the 'searchitem' value
+  //   if (searchItem) {
+  //     searchParams.set("searchItem", searchItem);
+  //   } else {
+  //     searchParams.delete("searchItem", searchItem);
+  //   }
 
-    // generate the new pathname with the updated search parameters
-    const newPathname = `${
-      window.location.pathname
-    }?${searchParams.toString()}`;
+  //   // generate the new pathname with the updated search parameters
+  //   const newPathname = `${
+  //     window.location.pathname
+  //   }?${searchParams.toString()}`;
 
-    router.push(newPathname);
-  };
+  //   router.push(newPathname);
+  // };
 
   return (
     <form onSubmit={handleSearch}>
